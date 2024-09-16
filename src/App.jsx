8c991a4d83d0cbe9acd9eb3aaccfa1f234ece5d0
Tushar Rana <tusharrana2505@gmail.com>
2024-09-16 16:54:12 +0530

    Navbar left component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Navbar from "./components/Navbar"
 
 function App() {
-
-
   return (
     <>
-    
+    <div className='w-full h-screen bg-zinc-950 text-white font-serif'>
+      <Navbar />
+    </div>
     </>
   )
 }
 
 export default App
+ 
