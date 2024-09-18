b7f7e14a28b0ecc7782a85d839650dc34d455a73
Tushar Rana <tusharrana2505@gmail.com>
2024-09-18 19:16:30 +0530

    Products componenets styling and video animation added using motion.div

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,31 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Button from './Button'
+/* eslint-disable react/prop-types */
+import React from "react"
+import Button from "./Button"
 
-function Product({val}) {
-  return (
-    <div>
-       <div className='w-full py-20 text-white '>
-      <div className='flex  max-w-screen-xl mx-auto items-center justify-between'>
-        <h1 className='text-6xl  capatalize font-medium '>{val.title}</h1>
-        <div className='dets w-1/3'>
-        <p>{val.description}</p>
-        <div className="flex items-center gap-5 mt-8">
+const Product = ({ val, mover, count , hovered}) => {
+
+    const bgcolour = hovered?"":"" ;
+
+
+    return (
+        <div className={`w-full h-[20rem] py-20 bg- ${bgcolour} text-white`}>
+            <div
+                onMouseEnter={() => mover(count) }
+                
+                className="max-w-screen-xl mx-auto flex items-center justify-between"
+            >
+                <h1 className="text-5xl font-semibold">{val.title}</h1>
+                <div className="dets w-1/3">
+                    <p className="mb-10">{val.description}</p>
+                    <div className="flex items-center gap-5">
                         {val.live && <Button />}
                         {val.case && <Button title="Case Study" />}
                     </div>
+                </div> 
+            </div>
         </div>
-      </div>
-    </div>
-
-    </div>
-  )
+    )
 }
 
 export default Product
